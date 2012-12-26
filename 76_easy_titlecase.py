4b94d33ba2202b19383108009d85a9293ae0b8a4
Ed Burnett <eburnett@gmail.com>
2012-12-25 19:12:08 -0800

    completed challenge 76 [easy]

---
+++ 76_easy_titlecase.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+# Challenge #76 [easy] Title case
+
+phrase = "the quick brown fox jumps over the lazy dog"
+exceptions = ['jumps', 'the', 'over']
+
+
+def titlecase(phrase, exceptions):
+    phrase_list = phrase.split()
+    for i in range(len(phrase_list)):
+        if phrase_list[i] not in exceptions or i == 0:
+            print(phrase_list[i].capitalize()),
+        else: 
+            print(phrase_list[i].lower()),
+        if i == phrase_list[-1]: print " "
+
+
+titlecase(phrase, exceptions)
