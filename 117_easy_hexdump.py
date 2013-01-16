aee95cb6a1fcabf8a131c7f83dc95ec1c76c67bc
Ed Burnett <eburnett@gmail.com>
2013-01-15 17:02:26 -0800

    hackey progress on #117

--- 117_easy_hexdump.py
+++ 117_easy_hexdump.py
@@ -4,16 +4,23 @@
 file = open('117_easy_hexdump.py', 'r')
 hexlist = []
 linecount = 0
+hexcount  = 0
+hexbegin = 0
 
 while file:
     try:
-        hexlist.append(hex(ord(file.read(1)))[2:].upper())
+        hexlist.append(hex(ord(file.read(1)))[2:])
     except TypeError:
         break
 
 for charcount,char in enumerate(hexlist):
     if (charcount % 16) == 0:
         linecount += 1
-    print hex(linecount)[2:], char
+        hexend = linecount * 16
+        currentline = []
+        currentline = hexlist[hexbegin*16:hexend]
+        hexbegin += 1
+        print hex(linecount)[2:], currentline
 
-# for every 16 items in hexlist, print linecount and items
+#if linecount == 1 or linecount % 16:
+#    print hex(linecount)[2:]
