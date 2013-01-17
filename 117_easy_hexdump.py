ed6ebc0d6b2147c69ca2655909d4f55551b3b1c4
Ed Burnett <eburnett@gmail.com>
2013-01-16 19:59:17 -0800

    More work on 117

--- 117_easy_hexdump.py
+++ 117_easy_hexdump.py
@@ -1,26 +1,35 @@
 # Challenge #117 [easy] Hexdump to ASCII
 # http://redd.it/16jiuq
 
-file = open('117_easy_hexdump.py', 'r')
-hexlist = []
-linecount = 0
-hexcount  = 0
-hexbegin = 0
+def hexconvert(filename):
+    """ Accept a filename and return a list of hex bytes """
+    hexlist = []
+    file = open(filename, 'r')
+    while file:
+        try:
+            hexlist.append(hex(ord(file.read(1)))[2:])
+        except TypeError:
+            break
+    return hexlist
 
-while file:
-    try:
-        hexlist.append(hex(ord(file.read(1)))[2:])
-    except TypeError:
-        break
 
-for charcount,char in enumerate(hexlist):
-    if (charcount % 16) == 0:
-        linecount += 1
-        hexend = linecount * 16
-        currentline = []
-        currentline = hexlist[hexbegin*16:hexend]
-        hexbegin += 1
-        print hex(linecount)[2:], currentline
+def linecounter(bytelist):
+    """ Return a list of formatted line numbers """
+    linecount = 0
+    result = []
+    for charcount, char in enumerate(bytelist):
+        if (charcount % 16) == 0:
+            linecount += 1
+            result.append(hex(linecount)[2:])
+    return result
 
-#if linecount == 1 or linecount % 16:
-#    print hex(linecount)[2:]
+#for charcount,char in enumerate(hexlist):
+#    if (charcount % 16) == 0:
+#        linecount += 1
+#        hexend = linecount * 16
+#        currentline = []
+#        currentline = hexlist[hexbegin*16:hexend]
+#        hexbegin += 1
+#        print hex(linecount)[2:], currentline
+
+print linecounter(hexconvert('117_easy_hexdump.py'))
