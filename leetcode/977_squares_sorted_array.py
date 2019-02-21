9e337d71ca218ccddd37d512bb9e92d090cc2a68
edmondburnett <_@edmondburnett.com>
2019-02-20 18:12:15 -0800

    sorted squares

---
+++ leetcode/977_squares_sorted_array.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""Given an array of integers A sorted in non-decreasing order, return an array
+of the squares of each number, also in sorted non-decreasing order.
+"""
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, A: 'List[int]') -> 'List[int]':
+        squares = []
+        for i in A:
+            squares.append(i**2)
+        squares.sort()
+        return squares
+
+    def sortedSquares2(self, A: 'List[int]') -> 'List[int]':
+        return sorted(i * i for i in A)
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.sortedSquares([-4, -1, 0, 3, 10]))
+    print(sol.sortedSquares([-7, -3, 2, 3, 11]))
+
+    print(sol.sortedSquares2([-4, -1, 0, 3, 10]))
+    print(sol.sortedSquares2([-7, -3, 2, 3, 11]))
