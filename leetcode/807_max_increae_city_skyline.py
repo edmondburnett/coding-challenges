2e2c1fcd371dda6ed478c81da30bccab78d9ad89
Edmond Burnett <_@edmondburnett.com>
2019-02-22 21:02:59 -0800

    laptop

--- leetcode/807_max_increae_city_skyline.py
+++ leetcode/807_max_increae_city_skyline.py
@@ -3,7 +3,7 @@
 """In a 2 dimensional array grid, each value grid[i][j] represents the height
 of a building located there. We are allowed to increase the height of any
 number of buildings, by any amount (the amounts can be different for different
-buildings). Height 0 is considered to be a building as well. 
+buildings). Height 0 is considered to be a building as well.
 
 At the end, the "skyline" when viewed from all four directions of the grid,
 i.e. top, bottom, left, and right, must be the same as the skyline of the
