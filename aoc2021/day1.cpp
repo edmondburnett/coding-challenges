656acac3e340cc9d7e246aa099dff142a22264c3
edmondburnett <_@edmondburnett.com>
2021-12-02 10:23:37 -0800

    Add Advent of Code 2021 + day 1 problem

---
+++ aoc2021/day1.cpp
@@ -0,0 +1,29 @@
+/*
+AoC 2021 - Day 1
+clang++ -std=c++11 day1.cpp -o day1 && ./day1
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream input_stream("day1_input.txt");
+    std::string line;
+    int value;
+    int prev_value;
+    int count = 0;
+
+    while(std::getline(input_stream, line))
+    {
+        prev_value = value;
+        value = std::stoi(line);
+
+        if (prev_value > 1 && value > prev_value)
+        {
+            count++;
+        }
+    }
+    std::cout << count << std::endl;
+
+}
