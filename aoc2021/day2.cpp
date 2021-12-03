a56f015b4aad1f981f088afd5148efcd0f5b02cd
edmondburnett <_@edmondburnett.com>
2021-12-03 10:25:37 -0800

    Completed day 2

--- aoc2021/day2.cpp
+++ aoc2021/day2.cpp
@@ -9,17 +9,16 @@ clang++ -std=c++11 day2.cpp -o day2 && ./day2
 #include <sstream>
 #include <vector>
 
-std::ifstream input_stream("day2_input.txt");
-
-int part1() {
-    std::string move, line;
+int part1(std::ifstream* input_stream) {
+    std::string line;
     int horiz = 0, depth = 0, amount; 
 
-    while(std::getline(input_stream, line))
+    while(std::getline(*input_stream, line))
     {
         std::istringstream iss(line);
         std::vector<std::string> move(std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>());
         amount = std::stoi(move[1]);
+    
         if (move[0] == "forward") {
             horiz += amount;
         } else if (move[0] == "down") {
@@ -31,10 +30,38 @@ int part1() {
     return horiz * depth;
 }
 
-int part2() {
+int part2(std::ifstream* input_stream) {
+    std::string line;
+    int horiz = 0, depth = 0, aim = 0, amount; 
 
+    while(std::getline(*input_stream, line))
+    {
+        std::istringstream iss(line);
+        std::vector<std::string> move(std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>());
+        amount = std::stoi(move[1]);
+    
+        if (move[0] == "forward") {
+            horiz += amount;
+            depth = depth + (aim * amount);
+        } else if (move[0] == "down") {
+            aim += amount;
+        } else if (move[0] == "up") {
+            aim -= amount;
+        } 
+    }
+    return horiz * depth;
 }
 
 int main() {
-    std::cout << part1() << std::endl;
+    std::ifstream input_stream("day2_input.txt");
+
+    int result1 = part1(&input_stream);
+    std::cout << "Part 1 Result: " << result1 << std::endl;
+    
+    // reset the file position to the beginning
+    input_stream.clear();
+    input_stream.seekg(0);
+
+    int result2 = part2(&input_stream);
+    std::cout << "Part 2 Result: " << result2 << std::endl;
 }
